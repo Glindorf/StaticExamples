59c73f4e275e2a865aa008006f693c2e1ee3c825
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:28:39 +0200

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,54 @@
+﻿namespace StaticExamples
+{
+    /// <summary>
+    /// A very simple representation of a car
+    /// </summary>
+    public class Car
+    {
+        #region Instance fields
+        private string _licensePlate;
+        private int _price;
+
+        private static int _noOfCars;
+
+        private static int _noOfLicensePlates = 0;
+
+        private static int _noOfPrice = 0;
+        
+        #endregion
+
+        #region Constructor
+        public Car(string licensePlate, int price)
+        {
+            _licensePlate = licensePlate;
+            _price = price;
+            _noOfCars = _noOfCars++;
+        }
+        #endregion
+
+        #region Properties
+        public string LicensePlate
+        {
+            get { _noOfLicensePlates++; return _licensePlate; }
+            set { _licensePlate = value; _noOfLicensePlates++;}
+        }
+
+        public int Price
+        {
+            get { _noOfPrice++; return _price; }
+            set { _price = value; _noOfPrice++;}
+        } 
+
+
+        #endregion
+
+        #region Methods
+        public static void PrintUsageStatistics()
+        {
+            System.Console.WriteLine($"Number of Cars: {_noOfCars}");
+            System.Console.WriteLine($"Number of License Plate properties: {_noOfLicensePlates}");
+            System.Console.WriteLine($"Number of Price properties: {_noOfPrice}");
+        }
+        #endregion
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticExamples
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Car CarA = new Car("Bil1", 123456);
+            Car CarB = new Car("Bil2", 123556);
+            Car CarC = new Car("Bil3", 143456);
+
+            CarB.LicensePlate = "blablabla";
+            System.Console.WriteLine(CarB.LicensePlate);
+
+            #region ListMethods test
+            List<int> aList = new List<int> { 23, 86, 51, 11, 39 };
+
+            int smallest = ListMethods.FindSmallestNumber(aList);
+            Console.WriteLine($"The smallest number in the list is : {smallest}");
+
+            int average = ListMethods.FindAverage(aList);
+            Console.WriteLine($"The average of the list is : {average}"); 
+            #endregion
+
+            Car.PrintUsageStatistics();
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
